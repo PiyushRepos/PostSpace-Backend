782c6c998e87b249d9cdc5aa5d85b9653f709bca
Piyush kumar <piyush2004.dev@gmail.com>
2024-11-02 22:02:18 +0530

    configure cors and add some express middlewares

--- src/app.js
+++ src/app.js
@@ -4,6 +4,19 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8000;
 import connectToDB from "./db/index.js";
+import cors from "cors";
+import path from "path";
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Database connection
 connectToDB()
