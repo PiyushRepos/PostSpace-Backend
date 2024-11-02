61f2cc9b88fd3bad736446f34ec74cf954adb284
Piyush kumar <piyush2004.dev@gmail.com>
2024-11-02 21:43:35 +0530

    server started

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
 import connectToDB from "./db/index.js";
 
 // Database connection
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error while starting server");
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is started at PORT ${PORT}`);
+    });
+  })
+  .catch((error) => console.log("MongoDB connection failed", error));
