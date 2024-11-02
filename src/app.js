afcb364b8467689efce38f3a150325b618f35d64
Piyush kumar <piyush2004.dev@gmail.com>
2024-11-02 23:17:44 +0530

    do some cahngse in app.js

--- src/app.js
+++ src/app.js
@@ -7,26 +7,19 @@ import connectToDB from "./db/index.js";
 import cors from "cors";
 import path from "path";
 
+// Database connection
+connectToDB();
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Database connection
-connectToDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.log("Error while starting server");
-      throw error;
-    });
-    app.listen(PORT, () => {
-      console.log(`Server is started at PORT ${PORT}`);
-    });
-  })
-  .catch((error) => console.log("MongoDB connection failed", error));
+app.listen(PORT, () => {
+  console.log(`Server is listening..`);
+});
