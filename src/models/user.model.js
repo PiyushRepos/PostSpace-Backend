1e9e17b8678a2e245de0fb7fde2219514a968ca7
Piyush kumar <piyush2004.dev@gmail.com>
2024-11-03 00:07:58 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+      required: [true, "Full name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim: true,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
